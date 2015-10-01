dd585faee454ac76235810d5096a0f4caa0afe7f
Jesper De Temmerman <jesper@toron.be>
2015-10-01 20:13:56 +0200

    Some correction to running jenkins and download the cli client jar

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,20 @@
-# jenkins needs at least Java 7
-apt-get install -y openjdk-7-jre openjdk-7-jdk
-
 # get the key, add the repository for jenkins and install
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 apt-get update
 apt-get install -y jenkins
+
+# jenkins needs at least Java 7
+apt-get install -y openjdk-7-jre openjdk-7-jdk
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+
+# restart so jenkins uses the correct java version
+/etc/init.d/jenkins restart
+
+# get the cli jar.
+# because it takes some time for the jenkins server to start and wget stops on connection issues,
+# do this in a loop until it succeeds but wait in between tries
+while true;do
+	wget http://localhost:8080/jnlpJars/jenkins-cli.jar && break
+	sleep 2
+done
